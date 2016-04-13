d466682369d6d7c230664e91f8858ab5c3ad11ae
cc <zangcc0474@utexas.edu>
2016-04-13 09:47:33 -0500

    Maps

--- GiftCardReminder/app/src/main/java/com/example/scmuncey/giftcardreminder/MapsActivity.java
+++ GiftCardReminder/app/src/main/java/com/example/scmuncey/giftcardreminder/MapsActivity.java
@@ -42,6 +42,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
     }
 }
