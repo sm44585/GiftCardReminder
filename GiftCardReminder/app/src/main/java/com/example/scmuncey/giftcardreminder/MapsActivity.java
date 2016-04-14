d92bc0b160a61a02eff9366ae32f0d9af20a801d
Spencer Muncey <scmuncey@yahoo.com>
2016-04-14 16:58:10 -0500

    Updated the Google Maps activity that Chenchao wrote to allow the user to search for locations given an address

--- GiftCardReminder/app/src/main/java/com/example/scmuncey/giftcardreminder/MapsActivity.java
+++ GiftCardReminder/app/src/main/java/com/example/scmuncey/giftcardreminder/MapsActivity.java
@@ -1,14 +1,28 @@
 package com.example.scmuncey.giftcardreminder;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.location.Address;
+import java.io.IOException;
+import java.util.List;
+
+import java.io.IOException;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
@@ -22,6 +36,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(getString(R.string.app_name), "Permission is granted");
+
+            } else {
+
+                Log.v(getString(R.string.app_name), "Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(getString(R.string.app_name), "Permission is granted");
+
+        }
     }
 
 
@@ -42,7 +72,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng domain_apple = new LatLng(30.401679, -97.726855);
         LatLng current_location = new LatLng(30.284891, -97.734014);
         mMap.addMarker(new MarkerOptions().position(domain_apple).title("Domain Apple Store"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(current_location));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
         //Add a map click listener
         mMap.setOnMapClickListener(this);
         //Set UI settings.
@@ -54,6 +94,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapClick(LatLng latLng) {
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.v(getString(R.string.app_name), "Permission: " + permissions[0] + "was " + grantResults[0]);
+            //resume tasks needing this permission
+        }
+    }
+
+    public void onSearch(View view) {
+        EditText location_tf = (EditText) findViewById(R.id.TFaddress);
+        String location = location_tf.getText().toString();
+        List<Address> addressList = null;
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Address address = addressList.get(0);
+            String properAddress = String.format("%s, %s",
+                    address.getMaxAddressLineIndex() > 0 ? address.getAddressLine(0) : "",
+                    address.getCountryName());
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            //mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(address.getLatitude(), address.getLongitude())).draggable(true)
+                    .title(properAddress));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        }
     }
 }
 
